8d23a8a3054f675c94560e44babe8ab921d651e6
VeraKulemina <verakulemina@gmail.com>
2023-03-17 20:40:49 -0600

    2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function getRealFloor(n) {
     return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
   }
+
+  function mergeArrays(arr1, arr2) {
+    return arr1.filter((item) => !arr2.includes(item)).concat(arr2).sort((a, b) => a - b)
+  }
